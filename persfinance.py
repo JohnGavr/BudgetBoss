20172c3069452ff0d6de4d295c5523bc5fe6b6e2
JohnGavr <gavriilidisglog@gmail.com>
2023-12-05 12:57:27 +0200

    Create tables and insert statement to the database

--- persfinance.py
+++ persfinance.py
@@ -85,5 +85,69 @@ try:
 except mysql.connector.Error as err:
     print(f"Database {sql_database} exists. We continue.")
 
+sql_connect.close()
+sql_cursor.close()
+
+### Create Table
+
+sql_connect= connect_to_mysql(sql_host,sql_user,sql_password,sql_database)
+sql_cursor= sql_connect.cursor()
+
+try:
+    sql_cursor.execute("CREATE TABLE finance (id INT AUTO_INCREMENT PRIMARY KEY, value VARCHAR(255), date DATE, kind BOOLEAN)")
+    print("Database finance, created succesfully!")
+except mysql.connector.Error as err:
+    print(f"The table exist, we continue.")
+
+# Ask user for Expense or Revenue
+
+def get_valid_input():
+    while True:
+        user_input = input("For Revenue press 0 and for expense press 1. Enter 0 or 1: ")
+        if user_input in ['0', '1']:
+            return int(user_input)
+        else:
+            print("Invalid input. Please enter either 0 or 1.")
+
+while True:
+
+    user_choice = get_valid_input()
+
+# Insert Date
+
+    date_input_str = input("Enter a date (YYYY-MM-DD): ")
+
+# Convert the string to a datetime object
+    try:
+        date_input = datetime.strptime(date_input_str, "%Y-%m-%d")
+    except ValueError:
+        print("Invalid date format. Please use the format YYYY-MM-DD.")
+
+
+# Insert Decimal number
+
+    user_input = input("Please enter a decimal number: ")
+
+    try:
+        decimal_number=float(user_input)
+        print(f"You entered: {decimal_number}")
+    except ValueError:
+    # Handle the case where the user didn't enter a valid decimal number
+        print("Invalid input. Please enter a valid decimal number.")
+
+
+
+
+    sql = "INSERT INTO finance (value, kind, date) VALUES (%s, %s, %s)"
+    val = (decimal_number, user_choice, date_input_str)
+
+    sql_cursor.execute(sql,val)
+
+    sql_connect.commit()
+
+    exit_command = input("Enter 'exit' to stop or any other key to continue: ")
+    if exit_command.lower() == 'exit':
+        break  # Exit the main loop if the user inputs 'exit'
+
 
 
